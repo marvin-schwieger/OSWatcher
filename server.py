fcd413ec48f3912ec4417b29c02bab1464e898f6
marvin-schwieger <marvin.sw@hotmail.com>
2025-07-11 03:11:07 +0200

    Creates Thread Routine for every client and runs infintly TODO: handle output file

--- server.py
+++ server.py
@@ -1,51 +1,51 @@
 import threading
 import socket 
 
-PORT = 2
+PORT = 5000
 SERVER = "localhost"
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
-DISCONNECT_MESSAGE = "!DISCONNECT"
-
-server = socket.create_server(ADDR)
 
 clients = set()
 clients_lock = threading.Lock()
 
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} Connected")
-
-    try:
-        connected = True
-        while connected:
+    print(conn, addr)
+    
+    while True:
+        try:
+            # Receive one message (up to 1024 bytes)
             msg = conn.recv(1024).decode(FORMAT)
-            if not msg:
-                break
-
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-
-            print(f"[{addr}] {msg}")
-            with clients_lock:
-                for c in clients:
-                    c.sendall(f"[{addr}] {msg}".encode(FORMAT))
-
-    finally:
-        with clients_lock:
-            clients.remove(conn)
-
-        conn.close()
-
+            if msg:
+                print(f"[{addr}] {msg}")
+            else:
+                print(f"[{addr}] No message received")
+        except Exception as e:
+            print(f"Error receiving message from {addr}: {e}")
+        finally:
+            # Remove client from set and close connection
+            #with clients_lock:
+            #    if conn in clients:
+            #        clients.remove(conn)
+            #conn.close()
+            #print(f"[CONNECTION CLOSED] {addr}")
+            print(clients)
 
 def start():
     print('[SERVER STARTED]!')
+    server = socket.create_server(ADDR, family=socket.AF_INET)
     server.listen()
+    thread_count = 0
     while True:
         conn, addr = server.accept()
+        
         with clients_lock:
-            clients.add(conn)
+            clients.add(addr)
+            
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-
+        thread_count += 1
+        print(thread_count)
 
 start()
